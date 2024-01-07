b48be2712e6726e94f0686b75b2a52b19c0d8685
SandraWarugongo <Sandra.wwarugongo@gmail.com>
2024-01-06 22:26:14 -0800

    Write a function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
-word = "Holberton"
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
-middle_word = word[1:-1]
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
+# 7-add_tuple.py
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """Add two tuples."""
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = 0, 0
+        else:
+            tuple_a = tuple_a[0], 0
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
